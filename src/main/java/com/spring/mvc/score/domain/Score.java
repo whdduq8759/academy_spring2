12ec497ad94e804d59705f994dd4b0dc22c9fa4b
whdduq8759 <84843442+whdduq8759@users.noreply.github.com>
2021-10-01 17:32:25 +0900

    스프링 사용법(응용해서 만들기2)

--- src/main/java/com/spring/mvc/score/domain/Score.java
+++ src/main/java/com/spring/mvc/score/domain/Score.java
@@ -1,38 +1,33 @@
 package com.spring.mvc.score.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
+@Setter @Getter
 @ToString
 public class Score {
 
     private int stuNum; //학번
-    private String name;
-    private int kor;
-    private int eng;
-    private int math;
-    private int total;
-    private double average;
+    private String name; //이름
+    private int kor; //국어점수
+    private int eng; //영어점수
+    private int math; //수학점수
+    private int total; //총점
+    private double average; //평균
 
-
-    //너무 졸려요...
-    // 순차적 학번 부여 정적필드
+    //순차적 학번 부여 정적필드
     private static int seq;
 
     public Score() {
         this.stuNum = ++seq;
-        calcTotal();
-
     }
 
     public void calcTotal() {
         this.total = this.kor + this.eng + this.math;
         this.average = Math.round((this.total / 3.0) * 100) / 100.0;
-    };
-
+    }
 
     public Score(String name, int kor, int eng, int math) {
         this();
@@ -40,5 +35,6 @@ public class Score {
         this.kor = kor;
         this.eng = eng;
         this.math = math;
+        calcTotal();
     }
 }
