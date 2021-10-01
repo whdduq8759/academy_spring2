d62f8cd02629b0d8a7188187a4252431760c103e
whdduq8759 <84843442+whdduq8759@users.noreply.github.com>
2021-10-01 17:30:57 +0900

    Update ScoreController.java

--- src/main/java/com/spring/mvc/score/controller/ScoreController.java
+++ src/main/java/com/spring/mvc/score/controller/ScoreController.java
@@ -5,6 +5,7 @@ import com.spring.mvc.score.repository.ScoreRepository;
 import com.spring.mvc.score.service.ScoreService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +14,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
 
 @Controller
-@Log4j2 // 로그 출력을 도와주는 기능
-@RequiredArgsConstructor // final필드를 초기화하는 생성자 자동생성
+@Log4j2 //로그 출력을 도와주는 기능
+@RequiredArgsConstructor //final필드를 초기화하는 생성자 자동생성
 public class ScoreController {
 
     private final ScoreRepository scoreRepository;
     private final ScoreService scoreService;
-   /* @Autowired
-    public ScoreController(ScoreRepository scoreRepository) {
-        this.scoreRepository = scoreRepository;
-    }*/
 
-    // 점수 프로그램 화면요청
+//    @Autowired
+//    public ScoreController(ScoreRepository scoreRepository) {
+//        this.scoreRepository = scoreRepository;
+//    }
+
+    //점수프로그램 화면요청
     @GetMapping("/score/list")
     public String scoreList(Model model) {
         List<Score> scores = scoreRepository.findAll();
@@ -35,7 +37,7 @@ public class ScoreController {
     //점수정보 저장 요청
     @PostMapping("/score/register")
     public String register(Score score) {
-        log.info("점수 등록 요청!! - " + score);
+        log.info("점수 등록 요청! - " + score);
         scoreService.register(score);
         //리다이렉트는 재요청기능입니다.
         return "redirect:/score/list";
@@ -48,5 +50,5 @@ public class ScoreController {
         scoreRepository.remove(stuNum);
         return "redirect:/score/list";
     }
-}
 
+}
